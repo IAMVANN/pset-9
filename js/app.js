93219c506a3326f5997abd3990d99c1de6c75f9e
Ivan Zheng <IZheng@ucvts.tec.nj.us>
2020-02-11 10:36:14 -0500

    added start button

--- js/app.js
+++ js/app.js
@@ -1,15 +1,30 @@
 ///////////////////// CONSTANTS /////////////////////////////////////
 const brick = {
-    value : 1;
+    value : 1
 }
 const paddle = {
-    center : undefined;
+    center : undefined
 }
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
-let breakboard = getElementById('bREAKBOARD');
-window.onload
+
+let start;
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
-
+let breakboard = document.getElementById('bREAKBOARD');
+let breaker = document.getElementById("bREAK");
 ///////////////////// EVENT LISTENERS ///////////////////////////////
-
+window.onload = function(){
+    start = document.createElement("h2");
+    start.id = "start";
+    start.innerHTML = "New text!";
+    breaker.append(start);
+}
+breaker.onclick = init;
 ///////////////////// FUNCTIONS /////////////////////////////////////
+function init(object){
+    let thingy = object.target;
+    if(thingy == start){
+    } else {
+        return;
+    }
+    remove start;
+}
