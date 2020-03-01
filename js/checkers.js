29e3a76e2b707c8eb7569a2180963cf3f725fec0
IAMVANN <izheng@ucvts.org>
2020-02-29 21:36:12 -0500

    worked on checkers

--- js/checkers.js
+++ js/checkers.js
@@ -10,9 +10,22 @@ const initArray = [
     [2, 0, 2, 0, 2, 0, 2, 0]
 ]
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
-console.log(initArray)
+let array;
+let win;
+let lose;
+let startTurn = "player1";
+let turn;
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
-
+let board = document.getElementById("board");
+let boardArray = board.childNodes;
+console.log(board);
 ///////////////////// EVENT LISTENERS ///////////////////////////////
+window.onload = init;
+function init(){
+    array = initArray;
+    turn = startTurn;
+
+}
+
 
 ///////////////////// FUNCTIONS /////////////////////////////////////
