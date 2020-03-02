d8e06f358c46d3f28f7e780d0eb975200dfd48e2
Ivan Zheng <IZheng@ucvts.tec.nj.us>
2020-03-02 10:49:39 -0500

    js

--- js/checkers.js
+++ js/checkers.js
@@ -1,55 +1,74 @@
 ///////////////////// CONSTANTS /////////////////////////////////////
-const initArray = [
-    [0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [2, 0, 2, 0, 2, 0, 2, 0],
-    [0, 2, 0, 2, 0, 2, 0, 2],
-    [2, 0, 2, 0, 2, 0, 2, 0]
-]
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
 let array;
 let win;
 let lose;
 let startTurn = "player1";
 let turn;
+let yarray;
+let xarray;
+let none = new Object();
+let player1 = new Object();
+let player2 = new Object();
 let pArray;//piece array
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
 let board = document.getElementById("Checkboard");
+let canvas = board.getContext("2d");
 ///////////////////// EVENT LISTENERS ///////////////////////////////
 window.onload = init;
-board.onclick = move();
+
 
 
 
 ///////////////////// FUNCTIONS /////////////////////////////////////
 function init(){
-    array = initArray;
+    array = [
+        [none, player1, none, player1, none, player1, none, player1],
+        [player1, none, player1, none, player1, none, player1, none],
+        [none, player1, none, player1, none, player1, none, player1],
+        [none, none, none, none, none, none, none, none],
+        [none, none, none, none, none, none, none, none],
+        [player2, none, player2, none, player2, none, player2, none],
+        [none, player2, none, player2, none, player2, none, player2],
+        [player2, none, player2, none, player2, none, player2, none]
+    ]
     array.forEach((item, i) => {
-        let y = 64 * i;
-        pArray[y].yval = y;
+        let y = 80 * i;
+        item.yval = y;
         item.forEach((object, o) => {
-            let x = 64 * o;
-            pArray[y][x].xval = x;
-            pArray[y][x].yval = y;
-            if(object == 0){
-                pArray[y][x].value = "none";
-            } else if(object == 1){
-                pArray[y][x].value = "player1";
-            } else if(object == 2){
-                pArray[y][x].value = "player2";
-            }
-            console.log(pArray[y][x])
+            let x = 80 * o;
+            object.xval = x;
+            object.yval = y;
         });
-
-
-
+    });
+    turn = startTurn;
+    render();
+}
+function render(){
+    array.forEach((item, i) => {
+        item.forEach((object, o) => {
+            let b;
+            if(i == 0){
+                 b = 0 + o;
+            } else{
+                 b = i * 8 + o;
+            }
+            if(b % 2 == 0){
+                canvas.beginPath();
+                console.log(object.xval + ', '+ object.yval)
+                canvas.rect(object.xval , object.yval, 80, 80);
+                canvas.fillStyle = "black";
+                canvas.fillRect(object.xval , object.yval, 80, 80);
+                canvas.stroke;
+            } else {
+                canvas.beginPath();
+                console.log(object.xval + ', '+ object.yval);
+                canvas.rect(object.xval , object.yval, 80, 80);
+                canvas.fillStyle = "Red";
+                canvas.fillRect(object.xval , object.yval, 80, 80);
+                canvas.stroke;
+            }
+        });
     });
 
-    turn = startTurn;
-}
-function move(e){
-    let target = e.target;
 }
