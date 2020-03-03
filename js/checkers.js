4587420d96ae0e6605577011aa760b457e130f5f
IAMVANN <izheng@ucvts.org>
2020-03-02 19:40:41 -0500

    checkers redo

--- js/checkers.js
+++ js/checkers.js
@@ -1,74 +1,79 @@
 ///////////////////// CONSTANTS /////////////////////////////////////
+const dinoposition = [1,3,5,7,8,10,12,14,17,19,21,23];
+const sharkposition = [62, 60, 58, 56, 55, 53, 51, 49, 46, 44, 42, 40]
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
 let array;
 let win;
 let lose;
-let startTurn = "player1";
+let shark = document.createElement("img");
+shark.src = "img/SHARK.png";
+shark.style.height = '60px';
+shark.style.width  = '60px';
+let dinosaur = document.createElement("img");
+dinosaur.src = "img/DINOSAUR.jpg";
+dinosaur.style.height = '60px';
+dinosaur.style.width  = '60px';
+let startTurn = "dino";
 let turn;
-let yarray;
-let xarray;
-let none = new Object();
-let player1 = new Object();
-let player2 = new Object();
-let pArray;//piece array
+let pArray;
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
 let board = document.getElementById("Checkboard");
-let canvas = board.getContext("2d");
+let dArray = [];
+let sArray = [];
+
 ///////////////////// EVENT LISTENERS ///////////////////////////////
 window.onload = init;
+board.onclick = takeTurn();
+let spots = document.getElementsByClassName("spot");
+
 
 
 
 
 ///////////////////// FUNCTIONS /////////////////////////////////////
 function init(){
-    array = [
-        [none, player1, none, player1, none, player1, none, player1],
-        [player1, none, player1, none, player1, none, player1, none],
-        [none, player1, none, player1, none, player1, none, player1],
-        [none, none, none, none, none, none, none, none],
-        [none, none, none, none, none, none, none, none],
-        [player2, none, player2, none, player2, none, player2, none],
-        [none, player2, none, player2, none, player2, none, player2],
-        [player2, none, player2, none, player2, none, player2, none]
-    ]
-    array.forEach((item, i) => {
-        let y = 80 * i;
-        item.yval = y;
-        item.forEach((object, o) => {
-            let x = 80 * o;
-            object.xval = x;
-            object.yval = y;
-        });
-    });
     turn = startTurn;
+    for(let i = 0; i < 12; i++ ){
+
+        var object = {
+                value : "dino",
+                img : dinosaur,
+                id : 1,
+                pos : dinoposition[i]
+            };
+        dArray.push(object);
+    }
+    for(let i = 0; i < 12; i++ ){
+        var object = {
+                value : "sharko",
+                img : shark,
+                id : 1,
+                pos : sharkposition[i]
+            };
+        sArray.push(object);
+    }
+
+    for(let i = 0; i < 63; i++){
+        spots[i].id = "id" + i;
+    }
     render();
 }
 function render(){
-    array.forEach((item, i) => {
-        item.forEach((object, o) => {
-            let b;
-            if(i == 0){
-                 b = 0 + o;
-            } else{
-                 b = i * 8 + o;
-            }
-            if(b % 2 == 0){
-                canvas.beginPath();
-                console.log(object.xval + ', '+ object.yval)
-                canvas.rect(object.xval , object.yval, 80, 80);
-                canvas.fillStyle = "black";
-                canvas.fillRect(object.xval , object.yval, 80, 80);
-                canvas.stroke;
-            } else {
-                canvas.beginPath();
-                console.log(object.xval + ', '+ object.yval);
-                canvas.rect(object.xval , object.yval, 80, 80);
-                canvas.fillStyle = "Red";
-                canvas.fillRect(object.xval , object.yval, 80, 80);
-                canvas.stroke;
-            }
-        });
+    dArray.forEach((item, i) => {
+        spots[dArray[i].pos].append(dArray[i].img);
+        console.log(spots[dArray[i].pos]);
+    });
+    sArray.forEach((item, i) => {
+        spots[sArray[i].pos].append(sArray[i].img);
     });
 
+}
+function takeTurn(e){
+    //console.log(e.target);
+    /*for(let b = 0; b < 63; b++){
+        if(spots[b] ==
+            console e.target){
+
+        }
+    }*/
 }
