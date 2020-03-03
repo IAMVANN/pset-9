661a16e9e23bb3ce9dd34805a55d70d3497121e5
IAMVANN <izheng@ucvts.org>
2020-03-02 21:06:15 -0500

    did sharks far left movement

--- js/checkers.js
+++ js/checkers.js
@@ -5,25 +5,21 @@ const sharkposition = [62, 60, 58, 56, 55, 53, 51, 49, 46, 44, 42, 40]
 let array;
 let win;
 let lose;
-let shark = document.createElement("img");
-shark.src = "img/SHARK.png";
-shark.style.height = '60px';
-shark.style.width  = '60px';
-let dinosaur = document.createElement("img");
-dinosaur.src = "img/DINOSAUR.jpg";
-dinosaur.style.height = '60px';
-dinosaur.style.width  = '60px';
+
+
 let startTurn = "dino";
 let turn;
 let pArray;
+let current;
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
 let board = document.getElementById("Checkboard");
 let dArray = [];
 let sArray = [];
-
+let return2 = false;
+let return3 = false;
 ///////////////////// EVENT LISTENERS ///////////////////////////////
 window.onload = init;
-board.onclick = takeTurn();
+board.onclick = takeTurn;
 let spots = document.getElementsByClassName("spot");
 
 
@@ -35,6 +31,11 @@ function init(){
     turn = startTurn;
     for(let i = 0; i < 12; i++ ){
 
+        let dinosaur = document.createElement("img");
+        dinosaur.src = "img/DINOSAUR.jpg";
+        dinosaur.style.height = '80px';
+        dinosaur.style.width  = '80px';
+
         var object = {
                 value : "dino",
                 img : dinosaur,
@@ -44,6 +45,12 @@ function init(){
         dArray.push(object);
     }
     for(let i = 0; i < 12; i++ ){
+
+        let shark = document.createElement("img");
+        shark.src = "img/SHARK.png";
+        shark.style.height = '80px';
+        shark.style.width  = '80px';
+
         var object = {
                 value : "sharko",
                 img : shark,
@@ -61,19 +68,96 @@ function init(){
 function render(){
     dArray.forEach((item, i) => {
         spots[dArray[i].pos].append(dArray[i].img);
-        console.log(spots[dArray[i].pos]);
+
     });
     sArray.forEach((item, i) => {
-        spots[sArray[i].pos].append(sArray[i].img);
+        spots[sArray[i].pos].appendChild(sArray[i].img);
     });
 
 }
 function takeTurn(e){
-    //console.log(e.target);
-    /*for(let b = 0; b < 63; b++){
-        if(spots[b] ==
-            console e.target){
 
+    target = e.target.parentElement;
+
+
+    for(let b = 0; b < 64; b++){
+        if(spots[b] == target){
+
+            if (turn == "dino") {
+                for(let i = dArray.length - 1; i >= 0; i--){
+                    if(dArray[i].pos == b){
+                        console.log("asdf")
+                        current = dArray[i].pos;
+                        highlighter("dino");
+                    }
+                }
+
+            } else if (turn == "shark"){
+
+            }
         }
-    }*/
+    }
+}
+function highlighter(who){
+    if(who == "dino"){
+        if(current % 8 == 0){
+            //highlight
+            let value = [current + 9];
+
+            dArray.forEach((item, i) => {
+
+                if(item.pos == value[0]){
+                    return2 = true;
+                    return ;
+                }
+            });
+            if(return2 == true){
+                return2 = false;
+                return;
+            }
+            console.log("asf")
+            sArray.forEach((item, i) => {
+
+                if(item.pos == value[0]){
+                    sArray.forEach((object, o) => {
+                        if(object.pos == value[0] + 9){
+                            return3 = true;
+                            return;
+                        }
+                    });
+                    if(return3 == true){
+                        return2 = true;
+                        return3 == false;
+                        return;
+                    }
+
+
+                    dArray.forEach((object, o) => {
+                        if(object.pos == value[0] + 9){
+                            return2 = true;
+                            return;
+                        }
+                    });
+                    value[0] = value[0] + 9;
+                    highlight(value[0]);
+
+
+                }
+
+            });
+            if(return2 == true){
+                return2 = false;
+                return;
+            }
+
+        } else if(current % 7 == 0){
+            let value = [current + 7];
+        } else {
+            let value = [current + 7, current + 9];
+        }
+    }
+
+}
+function highlight(value){
+    spots[value].style.backgroundColor = "gray";
 }
